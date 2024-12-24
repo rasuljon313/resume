9fa3e8df319285664f00e13c47d1bd1382f8a989
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-12-25 00:21:29 +0500

    add

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,14 @@
 import { Mail, Globe, Phone, BadgeCheck } from 'lucide-react';
+import { CiUser } from 'react-icons/ci';
 
 export default function Profile() {
   return (
     <div className="profile">
       <div className="profile__header">
-        <img
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=crop&w=256&h=256&q=80"
-          alt="Profile"
-          className="profile__image"
-        />
+      <CiUser className='profile__svg' />
         <div className="profile__info">
           <div className="profile__title">
-            <h1>Henry Walker</h1>
+            <h1>Anvarov Rasuljon</h1>
             <BadgeCheck className="profile__badge" size={24} />
           </div>
         </div>
@@ -20,9 +17,17 @@ export default function Profile() {
       <div className="profile__content">
         <div className="profile__section">
           <h2 className="section__heading">About</h2>
-          <p>Brooklyn based full-stack software engineer with 7+ years of experience in building web and mobile apps.</p>
+          <div className='profile__content__box'>
+            <p className='profile__content__box__txt' >Dedicated frontend developer with a strong foundation in HTML, CSS, and JavaScript, gained through completing a 10-month program at Proweb IT School. Passionate about creating responsive and user-friendly websites. Eager to bring technical skills and creativity to a dynamic team.</p>
+          </div>
         </div>
 
+        <div className="profile__section">
+          <h2 className="section__heading">Education</h2>
+           <h3>Proweb IT School</h3>
+             <p className='section__heading__box__title'>Frontend Development Course</p>
+             <p className='section__heading__box__txt'>Duration: 10 months</p>
+        </div>
         <div className="profile__section">
           <h2 className="section__heading">Contact</h2>
           <div className="profile__contact">
