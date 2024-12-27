3fc811b6d4ec1c9220fe54ac555d2fc3199f1b41
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-12-27 23:32:56 +0500

    add

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Skills from './components/Skills';
 import Languages from './components/Languages';
 import Projects from './components/Projects';
 import './styles/main.scss';
+import { BiLogoGithub } from 'react-icons/bi';
 
 const App = () => {
   return (
@@ -18,10 +19,7 @@ const App = () => {
           <div className="intro">
             <h2 className="section__heading">Intro</h2>
             <p className="intro__text">
-              I'm Henry Walker, a product engineer from Brooklyn, New York City. My expertise lies in developing innovative and user-focused products for both web and mobile platforms, where I emphasize intuitive design and robust functionality.
-            </p>
-            <p className="intro__text">
-              Holding a Master's degree in Computer Science from the Nova University of Science and Technology, my journey began in a dynamic startup, progressing to lead positions in established tech firms.
+            I’m Anvarov Rasuljon, a front-end developer dedicated to building interactive and accessible web experiences. I’ve developed a keen eye for detail and a strong problem-solving mindset. My primary focus is creating websites that not only look good but work seamlessly across all devices. When I’m not coding, you’ll find me experimenting with new frameworks, reading about the latest web trends, or improving my design skills.
             </p>
           </div>
           
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,92 +1,44 @@
-// import { Calendar, User } from 'lucide-react';
-
-// const projects = [
-//   {
-//     title: 'Interactive Data Dashboard',
-//     image: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-//     date: 'Jan 24, 2024',
-//     role: 'Frontend Lead',
-//     tags: ['Internal Tools']
-//   },
-//   {
-//     title: 'Personal Finance Tracker',
-//     image: 'https://images.unsplash.com/photo-1554224155-8d04cb21cd6c?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-//     date: 'Mar 15, 2024',
-//     role: 'Frontend Dev',
-//     tags: ['Web App']
-//   },
-//   {
-//     title: 'Personal Finance Tracker',
-//     image: 'https://images.unsplash.com/photo-1554224155-8d04cb21cd6c?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-//     date: 'Mar 15, 2024',
-//     role: 'Frontend Dev',
-//     tags: ['Web App']
-//   }
-// ];
-
-// export default function Projects() {
-//   return (
-//     <div className="projects">
-//       <h2 className="section__heading">Projects</h2>
-//       <div className="projects__grid">
-//         {projects.map((project) => (
-//           <div key={project.title} className="projects__card">
-//             <a href="">
-//             <img src={project.image} alt={project.title} />
-//             </a>
-//             <div className="projects__content">
-//               <h3 className="projects__title">{project.title}</h3>
-//               <div className="projects__meta">
-//                 <div className="projects__meta-item">
-//                   <Calendar size={16} />
-//                   {project.date}
-//                 </div>
-//                 <div className="projects__meta-item">
-//                   <User size={16} />
-//                   {project.role}
-//                 </div>
-//                 <div className="projects__meta-item">
-//                 {project.tags.map((tag) => (
-//                   <span key={tag} className="projects__tag">{tag}</span>
-//                 ))}
-//               </div>
-//               </div>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-
-
-import { Calendar, User } from 'lucide-react';
+import img from "../img/IMAGE 2024-12-27 22:06:45.jpg";
 
 const projects = [
   {
     title: 'Cinephie',
-    image: '',
+    image: img,
     date: 'Jan 24, 2024',
     role: 'Frontend Lead',
-    tags: ['Internal Tools'],
-    url: 'https://example.com/project1' // Loyihaga havola
+    tags: ['React', 'Sass', 'JavaScript', "Axios", "Use-debounce", "zustand", "tanstack/react-query"],
+    description: `"Cinephie Website" - My Experience and Impressions
+    The process of creating the cinema website was both exciting and educational for me. Through this project, I learned how to combine web design, front-end, and back-end technologies. I designed the website's pages using HTML, CSS, and JavaScript. To enhance user experience (UX), I used the React framework, which made the site more interactive and faster.
+  
+    The developed website allows users to search for movies and discover them with ratings and reviews. Additionally, I added search and filtering features to help users easily find the movies they are looking for.`,
+    url: 'https://example.com/project1',
+    real: "Has not domain"
   },
   {
-    title: 'Personal Finance Tracker',
-    image: 'https://images.unsplash.com/photo-1554224155-8d04cb21cd6c?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-    date: 'Mar 15, 2024',
-    role: 'Frontend Dev',
-    tags: ['Web App'],
-    url: 'https://example.com/project2' // Loyihaga havola
+    title: 'Cinephie',
+    image: img,
+    date: 'Jan 24, 2024',
+    role: 'Frontend Lead',
+    tags: ['React', 'HTML', 'CSS', 'JavaScript'],
+    description: `"Cinema Website" - My Experience and Impressions
+    The process of creating the cinema website was both exciting and educational for me. Through this project, I learned how to combine web design, front-end, and back-end technologies. I designed the website's pages using HTML, CSS, and JavaScript. To enhance user experience (UX), I used the React framework, which made the site more interactive and faster.
+  
+    The developed website allows users to search for movies and discover them with ratings and reviews. Additionally, I added search and filtering features to help users easily find the movies they are looking for.`,
+    url: 'https://example.com/project2',
+    real: "Has not domain"
   },
   {
-    title: 'Personal Finance Tracker',
-    image: 'https://images.unsplash.com/photo-1554224155-8d04cb21cd6c?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-    date: 'Mar 15, 2024',
-    role: 'Frontend Dev',
-    tags: ['Web App'],
-    url: 'https://example.com/project3' // Loyihaga havola
+    title: 'Cinephie',
+    image: img,
+    date: 'Jan 24, 2024',
+    role: 'Frontend Lead',
+    tags: ['React', 'HTML', 'CSS', 'JavaScript'],
+    description: `"Cinema Website" - My Experience and Impressions
+    The process of creating the cinema website was both exciting and educational for me. Through this project, I learned how to combine web design, front-end, and back-end technologies. I designed the website's pages using HTML, CSS, and JavaScript. To enhance user experience (UX), I used the React framework, which made the site more interactive and faster.
+  
+    The developed website allows users to search for movies and discover them with ratings and reviews. Additionally, I added search and filtering features to help users easily find the movies they are looking for.`,
+    url: 'https://example.com/project3',
+    real: "Has not domain"
   }
 ];
 
@@ -101,21 +53,15 @@ export default function Projects() {
               <img src={project.image} alt={project.title} />
             </a>
             <div className="projects__content">
-              <h3 className="projects__title">{project.title}</h3>
-              <div className="projects__meta">
-                <div className="projects__meta-item">
-                  <Calendar size={16} />
-                  {project.date}
-                </div>
-                <div className="projects__meta-item">
-                  <User size={16} />
-                  {project.role}
-                </div>
-                <div className="projects__meta-item">
-                  {project.tags.map((tag) => (
-                    <span key={tag} className="projects__tag">{tag}</span>
-                  ))}
-                </div>
+              <div className="projects__content__box">
+                <h3 className="projects__title">{project.title}</h3>
+                <p>{project.real}</p> {/* Fixed: Using 'project.real' instead of 'projects.real' */}
+              </div>
+              <p className="projects__description">{project.description}</p>
+              <div className="projects__meta-title">
+                {project.tags.map((tag, index) => (
+                  <span key={index} className="projects__tag">{tag}</span>
+                ))}
               </div>
             </div>
           </div>
